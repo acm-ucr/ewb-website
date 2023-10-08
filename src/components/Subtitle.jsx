325ec0d0e38b884e61b8bc9363e0c2c131a3106f
allisonpham7 <allisonppham@gmail.com>
2023-10-07 17:51:23 -0700

    Edited subtitle

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -1,5 +1,11 @@
 const Subtitle = ({ title }) => {
-  return <div>Subtitle</div>;
+  return (
+    <div>
+      Subtitle
+      <h2>Testing subtitle</h2>
+      <p>Testing paragraph.</p>
+    </div>
+  );
 };
 
 export default Subtitle;
