0dc544c6b0c7acfa73259cdd1c06949427840d68
allisonpham7 <allisonppham@gmail.com>
2023-10-12 02:52:22 -0700

    Add Subtitle

--- src/components/Subtitle.jsx
+++ src/components/Subtitle.jsx
@@ -1,9 +1,14 @@
-const Subtitle = ({ title }) => {
+import React from "react";
+const Subtitle = ({ title = "| Motivation" }) => {
+  // const Subtitle = ({ title }) => {
+  const blueColor = "#1151D3";
+  const greenColor = "#80C393";
   return (
     <div>
-      Subtitle
-      <h2>Testing subtitle</h2>
-      <p>Testing paragraph.</p>
+      <h2 style={{ color: blueColor }}>{title}</h2>
+      <p style={{ color: greenColor }}>
+        Rural communities are already disadvantaged socioeconomically
+      </p>
     </div>
   );
 };
