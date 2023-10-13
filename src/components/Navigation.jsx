ac3b71ffe67654ed77b37efc20c7f52eb7c01702
jvarCS <vargassegoviano@gmail.com>
2023-10-12 23:32:54 -0700

    navbar add

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,82 @@
+"use client";
+import Navbar from "react-bootstrap/Navbar";
+import React from "react";
+import blueLogo from "../../public/assets/blueLogo.svg";
+import Link from "next/link";
+import Image from "next/image";
+import { items } from "../data/navigation";
+import Nav from "react-bootstrap/Nav";
+import { useState } from "react";
+import { NavDropdown } from "react-bootstrap";
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const [selected, setSelected] = useState("");
+  return (
+    <Navbar // Removed fixed="top"
+      collapseOnSelect
+      expand="md"
+      fixed="top"
+      className="w-full md:h-[8vh] flex pb-10 font-playfair !bg-slate-400 justify-between items-center"
+    >
+      <Link href="/" className="items-center ml-3">
+        <Image src={blueLogo} />
+      </Link>
+
+      <Navbar.Collapse className="items-center mr-12 md:justify-end justify-center">
+        <Nav className="mb-2 w-12/12 no-underline text-2xl flex items-center">
+          {items.map((item, index) => (
+            <Nav.Link
+              as={Link}
+              key={index}
+              href={item.link}
+              onClick={() => setSelected(item.name)} // Blue border when selected became squared immediately after selecting something else so applied rounded-full
+              className={`hover:cursor-pointer rounded-full mb-0 py-1 px-4 no-underline !text-black text-2xl whitespace-nowrap !font-normal hover:!text-blue-600 duration-300 ${
+                selected === item.name &&
+                "!bg-ewb-blue-200 rounded-full text-white hover:!text-white" // To remove blue hover i set new hover to white
+              }`}
+            >
+              {item.name === "PROJECTS" ? (
+                <NavDropdown title={item.name}>
+                  <NavDropdown.Item
+                    className="text-white hover:!bg-ewb-green !bg-ewb-blue-200"
+                    href="#"
+                  >
+                    Costa Rica
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    className="text-white hover:!bg-ewb-green !bg-ewb-blue-200"
+                    href="#"
+                  >
+                    Tanzania
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    className="text-white hover:!bg-ewb-green !bg-ewb-blue-200"
+                    href="#"
+                  >
+                    Water Quality and Filtration
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    className="text-white hover:!bg-ewb-green !bg-ewb-blue-200"
+                    href="#"
+                  >
+                    Robotics
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    className="text-white hover:!bg-ewb-green !bg-ewb-blue-200"
+                    href="#"
+                  >
+                    Air Quality System
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                item.name
+              )}
+            </Nav.Link>
+          ))}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Navigation;
---
+++ src/data/navigation.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    name: "ABOUT",
+    link: "about",
+  },
+  {
+    name: "PROJECTS",
+    link: "projects",
+  },
+  {
+    name: "EVENTS",
+    link: "events",
+  },
+  {
+    name: "BOARD",
+    link: "board",
+  },
+];
