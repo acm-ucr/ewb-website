603f16276f72b462cf4eae7a4779724dc792a631
Stanley Lew <stanley43595@gmail.com>
2023-10-13 10:24:48 -0700

    added title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,31 @@
-const Title = ({ subTitle, title }) => {
-  return <div>Title</div>;
+import React from "react";
+import { COLORS } from "../data/colors";
+const Title = ({ subTitle = "LEARN WHO WE ARE", title = "ABOUT US" }) => {
+  const titleWords = title.split(" ");
+  return (
+    <div>
+      <p
+        className={
+          COLORS.green.text + " font-semibold mb-0 flex justify-center"
+        }
+      >
+        {subTitle}
+      </p>
+      <h2 className={" mt-0 font-bold"}>
+        <span
+          className={
+            COLORS.blue.bg +
+            " text-white underline underline-offset-[14px] decoration-[#80C393] decoration-8"
+          }
+        >
+          {titleWords[0]}
+        </span>
+        <span className={COLORS.blue.text}>
+          {" " + titleWords.slice(1).join(" ")}
+        </span>
+      </h2>
+    </div>
+  );
 };
 
 export default Title;
