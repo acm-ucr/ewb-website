82a8d97ead16a6d2a35cd06c6b95a34f74889ec2
jvarCS <vargassegoviano@gmail.com>
2023-10-13 01:24:52 -0700

    navbar fix

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -16,7 +16,7 @@ const Navigation = () => {
       collapseOnSelect
       expand="md"
       fixed="top"
-      className="w-full md:h-[8vh] flex pb-10 font-playfair !bg-slate-400 justify-between items-center"
+      className="w-full md:h-[8vh] flex pb-10 font-playfair !bg-white justify-between items-center"
     >
       <Link href="/" className="items-center ml-3">
         <Image src={blueLogo} />
@@ -25,18 +25,21 @@ const Navigation = () => {
       <Navbar.Collapse className="items-center mr-12 md:justify-end justify-center">
         <Nav className="mb-2 w-12/12 no-underline text-2xl flex items-center">
           {items.map((item, index) => (
-            <Nav.Link
-              as={Link}
-              key={index}
-              href={item.link}
-              onClick={() => setSelected(item.name)} // Blue border when selected became squared immediately after selecting something else so applied rounded-full
-              className={`hover:cursor-pointer rounded-full mb-0 py-1 px-4 no-underline !text-black text-2xl whitespace-nowrap !font-normal hover:!text-blue-600 duration-300 ${
-                selected === item.name &&
-                "!bg-ewb-blue-200 rounded-full text-white hover:!text-white" // To remove blue hover i set new hover to white
-              }`}
-            >
-              {item.name === "PROJECTS" ? (
-                <NavDropdown title={item.name}>
+            <div key={index}>
+              {item.drop ? (
+                <NavDropdown
+                  onClick={() => setSelected(item.name)}
+                  title={
+                    <span
+                      className={`hover:cursor-pointer rounded-full mb-0 py-1 px-4 no-underline !text-black text-2xl whitespace-nowrap !font-normal hover:!text-blue-600 duration-300 ${
+                        selected === "PROJECTS" &&
+                        "!bg-ewb-blue-200 rounded-full text-white hover:!text-white" // To remove blue hover i set new hover to white
+                      }`}
+                    >
+                      {item.name}
+                    </span>
+                  }
+                >
                   <NavDropdown.Item
                     className="text-white hover:!bg-ewb-green !bg-ewb-blue-200"
                     href="#"
@@ -69,9 +72,20 @@ const Navigation = () => {
                   </NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                item.name
+                <Nav.Link
+                  as={Link}
+                  key={index}
+                  href={item.link}
+                  onClick={() => setSelected(item.name)} // Blue border when selected became squared immediately after selecting something else so applied rounded-full
+                  className={`hover:cursor-pointer rounded-full mb-0 py-1 px-4 no-underline !text-black text-2xl whitespace-nowrap !font-normal hover:!text-blue-600 duration-300 ${
+                    selected === item.name &&
+                    "!bg-ewb-blue-200 rounded-full text-white hover:!text-white" // To remove blue hover i set new hover to white
+                  }`}
+                >
+                  {item.name}
+                </Nav.Link>
               )}
-            </Nav.Link>
+            </div>
           ))}
         </Nav>
       </Navbar.Collapse>
--- src/data/navigation.js
+++ src/data/navigation.js
@@ -2,17 +2,21 @@ export const items = [
   {
     name: "ABOUT",
     link: "about",
+    drop: false,
   },
   {
     name: "PROJECTS",
     link: "projects",
+    drop: true,
   },
   {
     name: "EVENTS",
     link: "events",
+    drop: false,
   },
   {
     name: "BOARD",
     link: "board",
+    drop: false,
   },
 ];
