a438c4f2798f6a908c3b25d2fc822ee2516ddeef
Fuyuki Malahom <120443522+Blumacaw25@users.noreply.github.com>
2023-10-17 16:57:28 -0700

    Added Focus Component

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,9 @@ import About from "@/components/home/About";
 import Landing from "@/components/home/Landing";
 import Projects from "@/components/home/Projects";
 import WhyJoin from "@/components/home/Contacts";
+import { ICONS } from "../data/icons";
+import Focus from "../components/home/Focus";
+
 const Home = () => {
   return (
     <>
@@ -9,6 +12,12 @@ const Home = () => {
       <About />
       <WhyJoin />
       <Projects />
+      <Focus
+        title="OUTREACH"
+        description="Get connect with our community and make an impact on our society"
+        link="google.com"
+        icon={ICONS.handshake}
+      />
     </>
   );
 };
