7f19bd498849651e376b6f993e099e228825571d
Kevin Loritsch <kloritsch58@gmail.com>
2023-10-18 16:20:47 -0700

    kevin/event

--- src/components/home/Event.jsx
+++ src/components/home/Event.jsx
@@ -1,44 +1,29 @@
-// Color Passing In: blue -> blue, anything else -> green
-// Date Start: Month/Day/Start Time
-// Date End: End Time
-// Include AM / PM on time
-// Pass Month as ALL CAPS Three Letter Word
+import { COLORS } from "@/data/colors";
 
-const Event = ({ start, end, location, name, descripton, color }) => {
+const Event = ({ start, end, location, name, description, color }) => {
   const startDate = start.split("/");
 
-  let bgcolor = "";
-  if (color === "blue") {
-    bgcolor = "#1051D3";
-  } else {
-    bgcolor = "#80C393";
-  }
-
   return (
-    <div className=" bg-ewb-blue-100 w-4/6 inline-flex">
+    <div className=" bg-ewb-blue-100 w-1/2 inline-flex">
       <div
-        className="text-white text-center max-w-fit w-1/4 pt-6 pb-1"
-        style={{ backgroundColor: `${bgcolor}` }}
+        className={`text-white text-center max-w-fit w-48 pt-6 pb-1 ${COLORS[color].bg}`}
       >
-        <h1 className="font-bold text-6xl break-all px-12 align-middle">
+        <p className="font-bold text-6xl break-all px-4 align-middle ">
           {startDate[0]} {startDate[1]}
-        </h1>
-        <p className="font-normal text-2xl align-middle">
+        </p>
+        <p className="font-normal text-2xl pl-1 text-center align-middle">
           {startDate[2]}-{end}
         </p>
       </div>
 
       <div className="px-3 w-3/4">
         <div
-          className="pt-3 text-ewb-blue-200 text-[40px] font-bold leading-[48px]"
-          style={{ color: `${bgcolor}` }}
+          className={`pt-3 text-ewb-blue-200 text-4xl font-bold ${COLORS[color].text}`}
         >
           {name}
         </div>
-        <div className="leading-7 text-ewb-black font-bold text-[19px]">
-          {location}
-        </div>
-        <div className="leading-7 text-[19px]">{descripton}</div>
+        <div className="text-ewb-black font-bold text-lg">{location}</div>
+        <div className=" text-lg">{description}</div>
       </div>
     </div>
   );
