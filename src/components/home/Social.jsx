f62d7c5d6d067337aacfb46030deacc2a70d8fb5
Dylan Wong <user.name>
2023-10-18 12:35:51 -0700

    added Social

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,5 +1,28 @@
-const Social = ({ name, link }) => {
-  return <div>Social</div>;
+import Link from "next/link";
+
+import { BsEnvelope, BsFacebook } from "react-icons/bs";
+import { LINKS } from "@/data/links";
+
+const Social = ({ link = LINKS }) => {
+  return (
+    <div className="flex flex-row">
+      <Link
+        href={link[0].link}
+        className="no-underline text-ewb-blue-200 hover:text-ewb-blue-200 flex flex-col mx-4 items-center hover:-translate-y-0.5 duration-300"
+      >
+        <BsEnvelope className="text-4xl" />
+        <p className="font-semibold">EMAIL</p>
+      </Link>
+      <Link
+        target="_blank"
+        href={link[1].link}
+        className="no-underline text-ewb-blue-200 hover:text-ewb-blue-200 flex flex-col mx-4 items-center hover:-translate-y-0.5 duration-300"
+      >
+        <BsFacebook className="text-4xl" />
+        <p className="font-semibold">FACEBOOK</p>
+      </Link>
+    </div>
+  );
 };
 
 export default Social;
