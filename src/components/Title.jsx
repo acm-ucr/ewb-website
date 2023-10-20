93dc5d739391c67c5642daf56d6ad51195cb0737
Stanley Lew <stanley43595@gmail.com>
2023-10-20 13:31:47 -0700

    removed curly braces

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,9 +2,9 @@ import { COLORS } from "../data/colors";
 const Title = ({ subTitle, title }) => {
   const titleWords = title.split(" ");
   return (
-    <div className={"flex flex-col justify-center items-center"}>
+    <div className="flex flex-col justify-center items-center">
       <p className={COLORS.green.text + " font-semibold mb-0"}>{subTitle}</p>
-      <p className={" mt-0 font-bold text-3xl"}>
+      <p className="mt-0 font-bold text-3xl">
         <span
           className={
             COLORS.blue.bg +
