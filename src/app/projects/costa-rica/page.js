06b3bc782a6e773d6cd2e497873eebc7f811140e
Fuyuki Malahom <120443522+Blumacaw25@users.noreply.github.com>
2023-10-23 17:07:39 -0700

    Added Project-Header

--- src/app/projects/costa-rica/page.js
+++ src/app/projects/costa-rica/page.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../../../components/projects/Header";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      page
+      <Header
+        img="../../../../images/costaRica/CostaRicaHeader.webp"
+        title="Costa Rica"
+      />
+    </div>
+  );
 };
 
 export default page;
--- src/components/projects/Header.jsx
+++ src/components/projects/Header.jsx
@@ -1,5 +1,12 @@
+import Title from "../Title";
+
 const Header = ({ img, title }) => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img src={img} alt="Image" className="mb-2.5" />
+      <Title subTitle="" title={title} />
+    </div>
+  );
 };
 
 export default Header;
