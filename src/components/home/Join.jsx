98179f353d9cdd2fe447eb88b3bfc3e0671dc428
Stanley Lew <stanley43595@gmail.com>
2023-11-03 14:42:34 -0700

    removed div

--- src/components/home/Join.jsx
+++ src/components/home/Join.jsx
@@ -2,11 +2,9 @@ import Title from "../Title";
 import Social from "./Social";
 const Join = () => {
   return (
-    <div className="p-10">
+    <div className="p-10 flex flex-col gap-y-8">
       <Title subTitle="JOIN US AND GET INVOLVED" title="JOIN US" />
-      <div className="p-10">
-        <Social />
-      </div>
+      <Social />
     </div>
   );
 };
