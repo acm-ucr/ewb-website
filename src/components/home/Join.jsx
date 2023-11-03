7dc8937046156b1b0497659460e4678536865b02
Stanley Lew <stanley43595@gmail.com>
2023-11-03 00:46:25 -0700

    forgot to format code and stuff whoops

--- src/components/home/Join.jsx
+++ src/components/home/Join.jsx
@@ -2,9 +2,9 @@ import Title from "../Title";
 import Social from "./Social";
 const Join = () => {
   return (
-    <div className = "p-10">
-      <Title subTitle = "JOIN US AND GET INVOLVED" title = "JOIN US"/>
-      <div className = "p-10">
+    <div className="p-10">
+      <Title subTitle="JOIN US AND GET INVOLVED" title="JOIN US" />
+      <div className="p-10">
         <Social />
       </div>
     </div>
