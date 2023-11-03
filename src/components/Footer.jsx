6d2d66c6834589aaf149f2196e513e3f82270b26
Kevin Loritsch <kloritsch58@gmail.com>
2023-11-03 15:27:29 -0700

    kevin/footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,37 @@
 import whiteLogo from "../../public/assets/whiteLogo.svg";
 import Image from "next/image";
+import Link from "next/link";
 import { ICONS } from "@/data/icons";
+import { LINKS } from "@/data/links";
 
 const Footer = () => {
   return (
     <div className="w-full bg-ewb-blue-200 border-t-[20px] border-t-ewb-green pt-32 flex justify-between">
       <div className="inline-flex pl-6 pt-10 text-white font-bold text-xl">
         <Image src={whiteLogo} alt="EWB Logo" className="scale-125" />
-        <p className="pt-16 pl-4">EWB at UCR</p>
+        <p className="pl-4 pt-20">EWB at UCR</p>
       </div>
 
-      <div className="px-1 text-right text-white m-3 text-sm">
-        <div className="ml-3 scale-125 float-right">{ICONS["facebook"]}</div>
-        <div className="scale-125 float-right"> {ICONS["email"]} </div>
-        <p className="pt-4">900 University Ave</p>
-        <p>Riverside, CA 92521</p>
-        <p>United States</p>
-        <p>ewb.ucriverside@gmail.com</p>
-        <p>619-395-3534</p>
+      <div className="text-right text-white text-sm flex flex-col m-0 p-0">
+        <div className="flex flex-row scale-75 ml-20">
+          {LINKS.map((social, index) => (
+            <Link
+              key={index}
+              href={social.link}
+              className="no-underline text-white hover:text-ewb-white mx-1 flex flex-col items-center hover:-translate-y-0.5 duration-300"
+            >
+              <icons className="text-4xl">{ICONS[social.text]}</icons>
+              <div className="font-semibold uppercase">{social.text}</div>
+            </Link>
+          ))}
+        </div>
+        <div className="mx-4 pb-3">
+          <div>900 University Ave</div>
+          <div>Riverside, CA 92521</div>
+          <div>United States</div>
+          <div>ewb.ucriverside@gmail.com</div>
+          <div>619-395-3534</div>
+        </div>
       </div>
     </div>
   );
