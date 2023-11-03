dc71b8bba3f3db8d94609fe614480e5ffd32591a
Kevin Loritsch <kloritsch58@gmail.com>
2023-11-03 14:31:57 -0700

    kevin/footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,27 +4,20 @@ import { ICONS } from "@/data/icons";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-ewb-green">
-      <p className="leading-4"> &nbsp; </p>
+    <div className="w-full bg-ewb-blue-200 border-t-[20px] border-t-ewb-green pt-32 flex justify-between">
+      <div className="inline-flex pl-6 pt-10 text-white font-bold text-xl">
+        <Image src={whiteLogo} alt="EWB Logo" className="scale-125" />
+        <p className="pt-16 pl-4">EWB at UCR</p>
+      </div>
 
-      <div className="w-full bg-ewb-blue-200 border-t-8 border-t-ewb-green pt-32 flex justify-between">
-        <div className="inline-flex pl-6 pt-10 text-white font-bold text-xl">
-          <Image src={whiteLogo} alt="EWB Logo" className="scale-125" />
-          <p className="pt-16 pl-4">EWB at UCR</p>
-        </div>
-
-        <div className="px-1 text-right text-white m-3 text-sm">
-          <div className="ml-3 scale-125 float-right">
-            {" "}
-            {ICONS["facebook"]}{" "}
-          </div>
-          <div className="scale-125 float-right"> {ICONS["email"]} </div>
-          <p className="pt-4">900 University Ave</p>
-          <p>Riverside, CA 92521</p>
-          <p>United States</p>
-          <p>ewb.ucriverside@gmail.com</p>
-          <p>619-395-3534</p>
-        </div>
+      <div className="px-1 text-right text-white m-3 text-sm">
+        <div className="ml-3 scale-125 float-right">{ICONS["facebook"]}</div>
+        <div className="scale-125 float-right"> {ICONS["email"]} </div>
+        <p className="pt-4">900 University Ave</p>
+        <p>Riverside, CA 92521</p>
+        <p>United States</p>
+        <p>ewb.ucriverside@gmail.com</p>
+        <p>619-395-3534</p>
       </div>
     </div>
   );
--- src/components/home/Event.jsx
+++ src/components/home/Event.jsx
@@ -1,29 +1,24 @@
 import { COLORS } from "@/data/colors";
-
-const Event = ({ start, end, location, name, description, color }) => {
-  const startDate = start.split("/");
-
+const getTimeString = (date) => {
+  const dateArr = date.toLocaleTimeString().split(":");
+  return `${dateArr[0]}:${dateArr[1]}${dateArr[2].split(" ")[1]}`;
+};
+const Event = ({ start, end, location, name, descripton, color }) => {
   return (
-    <div className=" bg-ewb-blue-100 w-3/5 inline-flex">
+    <div className="bg-ewb-blue-100 flex w-full font-bold">
       <div
-        className={`text-white text-center w-1/4 aspect-square p-1 flex flex-col items-center justify-center ${COLORS[color].bg}`}
+        className={`text-4xl text-white text-center w-1/6 aspect-square flex flex-col items-center justify-center ${COLORS[color].bg}`}
       >
-        <p className="font-bold text-6xl break-all px-4 align-middle ">
-          {startDate[0]} {startDate[1]}
-        </p>
-        <p className="font-normal text-2xl pl-1 text-center align-middle">
-          {startDate[2]}-{end}
-        </p>
-      </div>
-
-      <div className="px-3 w-3/4">
-        <div
-          className={`pt-3 text-ewb-blue-200 text-4xl font-bold ${COLORS[color].text}`}
-        >
-          {name}
+        <div>{start.toString().split(" ")[1]}</div>
+        <div>{start.getDate()}</div>
+        <div className="font-normal text-lg">
+          {getTimeString(start)}- {getTimeString(end)}
         </div>
-        <div className="text-ewb-black font-bold text-lg">{location}</div>
-        <div className=" text-lg">{description}</div>
+      </div>
+      <div className="p-3 w-5/6 text-ewb-black ">
+        <div className={`text-2xl ${COLORS[color].text}`}>{name}</div>
+        <div className="text-base">{location}</div>
+        <div className="text-base font-normal">{descripton}</div>
       </div>
     </div>
   );
