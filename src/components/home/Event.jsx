bd5d8ebe541d5186fe3d41bc2fa098ede37da968
menthy-wu <wumengxuan0728@gmail.com>
2023-11-02 23:42:38 -0700

    fix styling

--- src/components/home/Event.jsx
+++ src/components/home/Event.jsx
@@ -1,44 +1,24 @@
-// Color Passing In: blue -> blue, anything else -> green
-// Date Start: Month/Day/Start Time
-// Date End: End Time
-// Include AM / PM on time
-// Pass Month as ALL CAPS Three Letter Word
-
+import { COLORS } from "@/data/colors";
+const getTimeString = (date) => {
+  const dateArr = date.toLocaleTimeString().split(":");
+  return `${dateArr[0]}:${dateArr[1]}${dateArr[2].split(" ")[1]}`;
+};
 const Event = ({ start, end, location, name, descripton, color }) => {
-  const startDate = start.split("/");
-
-  let bgcolor = "";
-  if (color === "blue") {
-    bgcolor = "#1051D3";
-  } else {
-    bgcolor = "#80C393";
-  }
-
   return (
-    <div className=" bg-ewb-blue-100 w-4/6 inline-flex">
+    <div className="bg-ewb-blue-100 flex w-full font-bold">
       <div
-        className="text-white text-center max-w-fit w-1/4 pt-6 pb-1"
-        style={{ backgroundColor: `${bgcolor}` }}
+        className={`text-4xl text-white text-center w-1/6 aspect-square flex flex-col items-center justify-center ${COLORS[color].bg}`}
       >
-        <h1 className="font-bold text-6xl break-all px-12 align-middle">
-          {startDate[0]} {startDate[1]}
-        </h1>
-        <p className="font-normal text-2xl align-middle">
-          {startDate[2]}-{end}
-        </p>
+        <div>{start.toString().split(" ")[1]}</div>
+        <div>{start.getDate()}</div>
+        <div className="font-normal text-lg">
+          {getTimeString(start)}- {getTimeString(end)}
+        </div>
       </div>
-
-      <div className="px-3 w-3/4">
-        <div
-          className="pt-3 text-ewb-blue-200 text-[40px] font-bold leading-[48px]"
-          style={{ color: `${bgcolor}` }}
-        >
-          {name}
-        </div>
-        <div className="leading-7 text-ewb-black font-bold text-[19px]">
-          {location}
-        </div>
-        <div className="leading-7 text-[19px]">{descripton}</div>
+      <div className="p-3 w-5/6 text-ewb-black ">
+        <div className={`text-2xl ${COLORS[color].text}`}>{name}</div>
+        <div className="text-base">{location}</div>
+        <div className="text-base font-normal">{descripton}</div>
       </div>
     </div>
   );
--- src/components/home/Events.jsx
+++ src/components/home/Events.jsx
@@ -1,5 +1,22 @@
+import { EVENTS } from "@/data/mockEvents";
+import Event from "./Event";
+
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div className="w-7/12 flex flex-col items-center justify-center gap-4">
+      {EVENTS.map((event, index) => (
+        <Event
+          key={index}
+          start={event.start}
+          end={event.end}
+          location={event.location}
+          name={event.name}
+          descripton={event.description}
+          color={event.color}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Events;
