c3a34272edf7b2815e0c06b886e5330769d6fdf9
menthy-wu <wumengxuan0728@gmail.com>
2023-11-02 23:51:24 -0700

    remove from page

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import About from "@/components/home/About";
 import Landing from "@/components/home/Landing";
 import Projects from "@/components/home/Projects";
 import WhyJoin from "@/components/home/Contacts";
-import Subtitle from "@/components/Subtitle";
+
 const Home = () => {
   return (
     <>
@@ -10,10 +10,6 @@ const Home = () => {
       <About />
       <WhyJoin />
       <Projects />
-      <Subtitle
-        title="Motivation"
-        descripton="  Rural communities are already disadvantaged socioeconomically"
-      />
     </>
   );
 };
