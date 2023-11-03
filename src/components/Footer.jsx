bd48652e22a1327bfb36c6ccd9fd74757ef7ef6d
Kevin Loritsch <kloritsch58@gmail.com>
2023-11-03 10:40:08 -0700

    kevin/footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,43 +7,24 @@ const Footer = () => {
     <div className="w-full bg-ewb-green">
       <p className="leading-4"> &nbsp; </p>
 
-      <div className="w-full bg-ewb-blue-200 border-t-8 border-ewb-green">
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <table className="w-full">
-          <tbody>
-            <tr>
-              <td className="w-1/2">
-                <div className="mt-6 mx-6">
-                  <div className="inline-flex">
-                    <Image
-                      src={whiteLogo}
-                      alt="EWB Logo"
-                      className="scale-125"
-                    />
-                    <div className="text-white pt-10 pl-4 font-bold text-xl">
-                      EWB at UCR
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td className="w-1/2 text-right text-white py-6 px-8 text-sm">
-                <div className="inline-flex scale-125 px-1">
-                  <div className="mx-2"> {ICONS["email"]} </div>{" "}
-                  <div> {ICONS["facebook"]} </div>
-                </div>
-                <div>900 University Ave</div>
-                <div>Riverside, CA 92521</div>
-                <div>United States</div>
-                <div>ewb.ucriverside@gmail.com</div>
-                <div>619-395-3534</div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="w-full bg-ewb-blue-200 border-t-8 border-t-ewb-green pt-32 flex justify-between">
+        <div className="inline-flex pl-6 pt-10 text-white font-bold text-xl">
+          <Image src={whiteLogo} alt="EWB Logo" className="scale-125" />
+          <p className="pt-16 pl-4">EWB at UCR</p>
+        </div>
+
+        <div className="px-1 text-right text-white m-3 text-sm">
+          <div className="ml-3 scale-125 float-right">
+            {" "}
+            {ICONS["facebook"]}{" "}
+          </div>
+          <div className="scale-125 float-right"> {ICONS["email"]} </div>
+          <p className="pt-4">900 University Ave</p>
+          <p>Riverside, CA 92521</p>
+          <p>United States</p>
+          <p>ewb.ucriverside@gmail.com</p>
+          <p>619-395-3534</p>
+        </div>
       </div>
     </div>
   );
