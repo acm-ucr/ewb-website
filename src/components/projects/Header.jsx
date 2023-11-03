34a29af939d1d3fbe4eab0f5dd64f54376fad16f
menthy-wu <wumengxuan0728@gmail.com>
2023-11-03 14:42:42 -0700

    add width full

--- src/components/projects/Header.jsx
+++ src/components/projects/Header.jsx
@@ -3,7 +3,7 @@ import Title from "../Title";
 const Header = ({ img, title }) => {
   return (
     <div>
-      <img src={img} alt="Image" className="mb-2.5" />
+      <img src={img} alt="Image" className="mb-2.5 w-full" />
       <Title subTitle="" title={title} />
     </div>
   );
