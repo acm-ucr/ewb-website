ce19910fd27f2f3ebc2f2e2d1e8de073bc0da81d
menthy-wu <wumengxuan0728@gmail.com>
2023-11-03 17:40:42 -0700

    fix some stiling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,25 +7,24 @@ import { LINKS } from "@/data/links";
 const Footer = () => {
   return (
     <div className="w-full bg-ewb-blue-200 border-t-[20px] border-t-ewb-green pt-32 flex justify-between">
-      <div className="inline-flex pl-6 pt-10 text-white font-bold text-xl">
-        <Image src={whiteLogo} alt="EWB Logo" className="scale-125" />
-        <p className="pl-4 pt-20">EWB at UCR</p>
+      <div className="flex items-end m-3 gap-3 text-white font-bold text-xl w-1/2">
+        <Image src={whiteLogo} alt="EWB Logo" />
+        <p>EWB at UCR</p>
       </div>
 
-      <div className="text-right text-white text-sm flex flex-col m-0 p-0">
-        <div className="flex flex-row scale-75 ml-20">
+      <div className="text-right text-white text-sm flex flex-col m-0 p-0 mx-4 w-1/2">
+        <div className="flex flex-row ml-20 self-end">
           {LINKS.map((social, index) => (
             <Link
               key={index}
               href={social.link}
               className="no-underline text-white hover:text-ewb-white mx-1 flex flex-col items-center hover:-translate-y-0.5 duration-300"
             >
-              <icons className="text-4xl">{ICONS[social.text]}</icons>
-              <div className="font-semibold uppercase">{social.text}</div>
+              <icons className="text-2xl">{ICONS[social.text]}</icons>
             </Link>
           ))}
         </div>
-        <div className="mx-4 pb-3">
+        <div className="py-2">
           <div>900 University Ave</div>
           <div>Riverside, CA 92521</div>
           <div>United States</div>
