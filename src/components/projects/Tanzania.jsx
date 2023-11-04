60599f10121f5b3e51c6732ce61aa56a2f08db38
Fuyuki Malahom <120443522+Blumacaw25@users.noreply.github.com>
2023-11-04 12:17:31 -0700

    Added Tanazania.jsx

--- src/components/projects/Tanzania.jsx
+++ src/components/projects/Tanzania.jsx
@@ -1,5 +1,119 @@
+import Subtitle from "../Subtitle";
+
 const Tanzania = () => {
-  return <div>Tanzania</div>;
+  return (
+    <div>
+      <div className="grid grid-cols-3 gap-10 m-28">
+        <div className="col-span-3 p-4">
+          <Subtitle title="General Overview" />
+          <div className="ml-2 mt-1">
+            In collaboration with a Civil Developer in Tanzania and the
+            professional chapter of E.W.B in L.A. We are looking to install a
+            micro controlled solar water pump system for a school, of 400+, in
+            Tanzania. Currently students have to make themselves down a
+            treacherous path of around 300 meters. This takes time out of their
+            studies and puts their health and safety at risk!
+          </div>
+        </div>
+        <div className="col-span-2 p-4">
+          <Subtitle title="Motivation" />
+          <div className="ml-2 mt-1">
+            <ul
+              className="marker:text-ewb-blue-200 ml-4"
+              style={{ listStyleType: "square" }}
+            >
+              <li>
+                Make an impact on underprivileged students by supplying them
+                with technology to overcome physical barriers to an education
+              </li>
+              <li>
+                Inspire them to continue their education through mentoring
+                programs
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img
+          className="w-full"
+          src="/images/tanzania/tanzania1.gif"
+          alt="image"
+        />
+        <div className="col-span-2 p-4">
+          <Subtitle title="Micro Controlled Solar Water Pump System" />
+          <div className="ml-2 mt-1">
+            <ul
+              className="marker:text-ewb-blue-200 ml-4"
+              style={{ listStyleType: "square" }}
+            >
+              <li>Install solar panels to provide power for water pump</li>
+              <li>Install water pump and all the required tubing</li>
+              <li>
+                Install two communication Micro Controller to monitor power
+                supply and water control
+              </li>
+              <li>
+                Provide tutorials/ learning exercises on water controlled pump
+                for maintenance
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img
+          className="w-full"
+          src="\images\tanzania\tanzania2.webp"
+          alt="image"
+        />
+        <div className="col-span-2 p-4">
+          <Subtitle
+            title="Educational Micro controller projects"
+            description="Education is very hard to obtain affecting isolated rural communities the most"
+          />
+          <div className="ml-2 mt-1">
+            <ul
+              className="marker:text-ewb-blue-200 ml-4"
+              style={{ listStyleType: "square" }}
+            >
+              <li>
+                Providing some micro controllers and electronic parts allow for
+                community members, from kids to adults, gain a valuable skill
+                that improves their future opportunities
+              </li>
+              <li>
+                Is beneficial for both the volunteer(you), to gain once a
+                lifetime experience, and to the disadvantage community by
+                improving their way of life
+              </li>
+              <li>
+                Please see{" "}
+                <a
+                  href="costa-rica"
+                  className="text-ewb-blue-200 font-bold underline hover:text-ewb-green"
+                >
+                  Costa Rica
+                </a>{" "}
+                page for examples on educational Micro Controller Projects
+              </li>
+              <li>
+                A water treatment system will also be implemented, to learn more
+                check out the{" "}
+                <a
+                  href="water-quality-and-filtration"
+                  className="text-ewb-blue-200 underline font-bold hover:text-ewb-green"
+                >
+                  water quality and filtration
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img
+          className="w-full"
+          src="\images\tanzania\tanzania3.gif"
+          alt="image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Tanzania;
