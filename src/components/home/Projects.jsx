d8ead89cdad0b924ac4946b45729751d6dc33da1
Kevin Loritsch <kloritsch58@gmail.com>
2023-11-10 22:34:20 -0800

    kevin/projectSection

--- src/components/home/Projects.jsx
+++ src/components/home/Projects.jsx
@@ -1,5 +1,37 @@
+import Title from "../Title";
+import { PROJECTS } from "@/data/projects";
+import Project from "@/components/home/Project";
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <Title subTitle="LEARN FROM EXPERIENCE" title="OUR PROJECTS" />
+      <br />
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        {PROJECTS.map((project, index) => (
+          <Project
+            key={index}
+            name={project.title}
+            description={project.description}
+            link={project.link}
+          />
+        ))}
+      </div>
+      <br />
+    </div>
+  );
 };
 
 export default Projects;
+
+// import Social from "./Social";
+// const Join = () => {
+//   return (
+//     <div className="p-10 flex flex-col gap-y-8">
+//
+//       <Social />
+//     </div>
+//   );
+// };
+
+// export default Join;
