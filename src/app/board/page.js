58285aa4e20f575edf1e08f5d65b9b5cbd48d898
menthy-wu <wumengxuan0728@gmail.com>
2023-11-20 00:28:44 -0800

    fix board

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -4,7 +4,7 @@ import Title from "@/components/Title";
 
 const page = () => {
   return (
-    <div className="relative w-full flex flex-col items-center mt-14">
+    <div className="relative w-full flex flex-col items-center mt-14 overflow-hidden">
       <div className="absolute w-1/2 aspect-square bg-ewb-blue-100/40 rounded-full -translate-y-14" />
       <Title title="MEET THE TEAM" />
       <div className="relative grid grid-cols-4 w-9/12 gap-8 mt-14">
