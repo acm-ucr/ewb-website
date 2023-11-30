8f0539e98efec8fcfccfb26fcc77a2f6fafbb9d5
dylanwong6605 <dylanwong6605@gmail.com>
2023-11-29 23:03:19 -0800

    added WaterQualityHeader

--- src/app/projects/water-quality-and-filtration/page.js
+++ src/app/projects/water-quality-and-filtration/page.js
@@ -1,8 +1,11 @@
 import WaterQuality from "@/components/projects/WaterQuality";
+import Header from "@/components/projects/Header";
+import header from "../../../../public/images/WaterQualityAndFiltration/WaterQualityAndFiltrationHeader.webp";
 
 const page = () => {
   return (
     <>
+      <Header img={header.src} title="Water Quality and Filtration" />
       <WaterQuality />
     </>
   );
